9e732f084c1ab36732f46de3721339225d6dd933
liruilong <920494630@qq.com>
2023-12-01 13:41:15 +0800

    加入搜索

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,29 @@
 import { defineUserConfig,defaultTheme  } from 'vuepress'
-
+import { searchPlugin } from '@vuepress/plugin-search'
 export default defineUserConfig({
   charset: 'utf-8',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  plugins: [
+    searchPlugin({
+      locales: {
+        '/': {
+          placeholder: 'Search',
+        },
+        '/zh/': {
+          placeholder: '搜索',
+        },
+      },
+    }),
+  ],
   theme: defaultTheme({
     // 默认主题配置
     
     navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
       {
         text: '首页',
         link: '/',
