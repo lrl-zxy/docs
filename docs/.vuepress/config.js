db9d7dd3f57e95bd4ede9dfae1c90d7fa64b70a0
liruilong <920494630@qq.com>
2023-12-01 13:28:59 +0800

    df

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,26 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig,defaultTheme  } from 'vuepress'
 
 export default defineUserConfig({
   charset: 'utf-8',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    // 默认主题配置
+    
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+    locales: {
+      '/': {
+        selectLanguageName: 'English',
+      },
+      '/zh/': {
+        selectLanguageName: '简体中文',
+      },
+    },
+    logo: '/images/logo.jpeg',
+  }),
 })
